ecaefafc64fa09bbc791f8eec59a6fe129527183
Vitaliy <vitaly.matyko@mail.ru>
2025-03-28 18:17:03 +0300

    Create my new project vitaly-portfolio-site

---
+++ src/client/App.tsx
@@ -0,0 +1,17 @@
+import AppContainer from '../client/styles/app/AppContainer.module.scss';
+
+function App() {
+	return (
+		<>
+			<div className={AppContainer.app}>
+				<div className={AppContainer.top_panel}></div>
+			</div>
+		</>
+	)
+}
+
+export default App
+
+
+
+
