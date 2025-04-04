ccd3e72f4a867dfa97c02203ea8fece49655f04c
Vitaliy <vitaly.matyko@mail.ru>
2025-04-04 18:09:04 +0300

    Changed the project folders structure, and added link of follow to my social media, and added function of animation on main page

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# resume
+🌎 VitalyPro Hub — это персональный сайт-резюме,
+созданный для демонстрации профессиональных навыков и достижений в области программирования.
+Проект представляет собой интерактивную платформу, где работодатели могут ознакомиться с опытом,
+выполненными проектами и контактной информацией для сотрудничества.
+
+🎯 Цель проекта:
+🔸 Подчёркнуть навыки и опыт в области веб-разработки, баз данных и автоматизации (JAVASCRIPT, React, API).
+🔸 Донести до работодателей информацию об умениях, технологиях и проектном опыте (react, Javascript, TYPESCRIPT, rest api, BACKEND).
+🔸 Облегчить контакт с работодателями и представить компактную площадку с всей важной информацией.
+
+📚 Основные разделы сайта:
+🎓 Обо мне — краткое и профессиональное описание скиллов и карьерного пути (frontend, React, javascript, BACKEND,Typescript).
+📂 Проекты — портфолио с описанием ключевых работ, технологий (REACT, javascript, Typescript, api, REST API, backend) и ссылок на репозитории.
+🛠️ Навыки — список технологий и языков программирования: JavaScript, react, TypeScript, html, sass, css, redux, node.js, express,node, API, Rest API, FRONTEND, BACKEND.
+📬 Контакты — способы связи: почта,telegramm, social Media, GitHub, LinkedIn.
+
+🧰 Технологии и стэк:
+🛠️ Frontend: HTML5, CSS3, javascript, React, TYPESCRIPT, git.
+🛠️ Backend: Node.js, EXPRESS, rest api.
+🛠️ Базы данных: MongoDB, PostgreSQL, MySQL.
+🛠️ Версионное управление: GIT, GitHub.
+
+📈 Планы по развитию:
+📌 Добавить блог для описания изученных технологий (react, JAVASCRIPT, API, rest api, typescript).
+📌 Интегрировать форму обратной связи.
+📌 Оптимизировать сайт для SEO.
+
+🔗 Проект: [VitaliyPro Hub (демо)](https://github.com/VitalyMatyko/vitaly-portfolio-site)
+🔗 Исходный код: [GitHub](https://github.com/VitalyMatyko/vitaly-portfolio-site)
+🌟 Мой GitHub: [GitHub](https://github.com/VitalyMatyko)
--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,19 +1,69 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contacts from './components/Contacts';
-import Navbar from './components/Navbar'
-import AppStyles from './styles/app/AppContainer.module.scss'
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
+import Contacts from './components/contacts/Contacts';
+import Navbar from './components/navbar/Navbar';
+import AppStyles from './styles/AppContainer.module.scss';
+import Animation from './styles/Animation.module.scss'
+import { useEffect, useState } from 'react';
+
+
 
 const App = () => {
+
+	const [showNavbar, setShowNavbar] = useState<boolean>(false);
+	const [showOutside, setShowOutside] = useState<boolean>(false);
+	const [showMiddle, setShowMiddle] = useState<boolean>(false);
+	const [showText, setShowText] = useState<boolean>(false);
+
+	const getShowAnimation = () => {
+		setTimeout(() => {
+			setShowNavbar(true);
+		}, 1000);
+		setTimeout(() => {
+			setShowOutside(true);
+		}, 2000);
+		setTimeout(() => {
+			setShowMiddle(true);
+		}, 3000);
+		setTimeout(() => {
+			setShowText(true);
+		}, 4000);
+	}
+
+	useEffect(() => {
+		getShowAnimation()
+	}, [])
+
+	const followLink = (event: React.MouseEvent<HTMLDivElement>) => {
+		const id = event.currentTarget.id;
+		if (!id) return;
+
+		const link = {
+			github: import.meta.env.VITE_GITHUB_LINK,
+			telegram: import.meta.env.VITE_TELEGRAM_LINK,
+			linkedin: import.meta.env.VITE_LINKEDIN_LINK
+		}[id];
+
+		if (link) {
+			return window.open(link);
+		} else {
+			return window.location.href = '/'
+		}
+	}
+
+
 	return (
 		<Router>
 			<div className={AppStyles.app}>
-				<Navbar />
+				<div className={showNavbar ? Animation.visible_animation : Animation.hidden_animation}>
+					<Navbar />
+				</div>
 				<Routes>
-					<Route path='/' element={<Home />} />
+					<Route path='/' element={
+						<Home showOutside={showOutside} showMiddle={showMiddle} showText={showText} followLink={followLink} />} />
 					<Route path='/about' element={<About />} />
 					<Route path='/projects' element={<Projects />} />
 					<Route path='/skills' element={<Skills />} />
